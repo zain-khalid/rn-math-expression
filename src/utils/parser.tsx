99a9600c3c5c212c3eea0fb0b295b31d3a538f9d
Zain Khalid <zain.khalid@xcorebit.com>
2024-04-10 23:19:56 +0500

    handle the sub equation inside the content string

--- src/utils/parser.tsx
+++ src/utils/parser.tsx
@@ -1,5 +1,27 @@
+const removeWhiteSpaceAtIndex = (inputString: String, index: number): String => {
+    if (index < 0 || index >= inputString.length) {
+        return inputString;
+    }
+    const beforeIndex = inputString.substring(0, index);
+    const afterIndex = inputString.substring(index + 1);
+    return beforeIndex + afterIndex;
+}
+
 const transformStringContent = (string: String): String => {
-    return string.replace(/\$\$/g, '$');
+    let result = string;
+
+    while(true){
+        if(result.indexOf("\\[") === -1 && result.indexOf("\\]") === -1){
+            break;
+        } else {
+            result = removeWhiteSpaceAtIndex(result, result.indexOf("\\[")+2);
+            result = result.replace("\\[", "$");
+            result = removeWhiteSpaceAtIndex(result, result.indexOf("\\]")-1);
+            result = result.replace("\\]", "$");
+        }
+    }
+    
+    return result.replace(/\$\$/g, '$');
 }
 
 export {
